e1a6e2bbcc6c85e7b25505e3442899584ea11a0c
rratkevich <ratkevichigor94@gmail.com>
2019-09-03 21:21:38 +0300

    My first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let pets = {
+    name: 'Kitty',
+    numberLegs: 4,
+    body: true,
+    petArr: [ {id:0, name: null}, ],   
+    my: {
+
+        cats: {
+            nameCats: {
+                cat1: 'fas',
+                cat2: 'Nat',
+                cat3: false,
+            },
+            
+        },
+        dogs: undefined,
+    },
+};
+
+alert( "age" in pets );
+console.log('hi');
+
+
